25f3e559eb902fc4bb2c2fcc0f2a942120385fcc
Esteban Garcia <estebangarcia2121@gmail.com>
2021-10-01 21:06:14 -0700

    Replaced editorconfig with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-  extends: 'erb',
+  plugins: ['prettier'],
+  extends: ['erb'],
   rules: {
-    // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
+    'prettier/prettier': ['error'],
   },
   parserOptions: {
     ecmaVersion: 2020,
@@ -13,7 +14,6 @@ module.exports = {
   },
   settings: {
     'import/resolver': {
-      // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
       node: {},
       webpack: {
         config: require.resolve('./.erb/configs/webpack.config.eslint.js'),
